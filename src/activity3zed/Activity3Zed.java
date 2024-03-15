5cf822c00c16c6467c869e0f7b226f3e1d73ce37
Lenovo <Lenovo@LAPTOP-H5VF19G9>
2024-03-15 15:48:48 +0800

    Activity 3

---
+++ src/activity3zed/Activity3Zed.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activity3zed;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Activity3Zed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double Celsius = 23;
+        double Fahrenheit = (Celsius * 9/5) + 32;
+        
+        System.out.println("Temperature in Fahrenheit is: " + Fahrenheit + "F");
+    }
+    
+}
+
+  
+    
+
